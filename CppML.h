186bccaf21f4ef57c92787146d8905f3dbaa0824
ZigaSajovic <ziga.sajovic@gmail.com>
2019-05-14 06:39:49 +0200

    CppML.h

---
+++ CppML.h
@@ -0,0 +1,12 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#include "CppML/Const.h"
+#include "CppML/ControlFlow.h"
+#include "CppML/Functional.h"
+#include "CppML/Identity.h"
+#include "CppML/List.h"
+#include "CppML/Range.h"
+#include "CppML/Sequence.h"
+#include "CppML/TypeTraits.h"

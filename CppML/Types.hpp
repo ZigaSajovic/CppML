a777ca1e4e61f6d8ba1f9aefa1e2fc0ce7e4f7e5
ZigaSajovic <ziga.sajovic@gmail.com>
2019-06-11 14:58:54 +0200

    None type and Any (wild card)

---
+++ CppML/Types.hpp
@@ -0,0 +1,19 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CPPML_TYPES_HPP
+#define CPPML_TYPES_HPP
+namespace ml {
+/*
+ * None:
+ * Represents a "nothing" type
+ */
+struct None {};
+/*
+ * _:
+ * Represent a wild card, or Any
+ */
+struct _ {};
+}; // namespace ml
+#endif

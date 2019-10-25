c5bfc679e82d53e3526ea60e67dc93d0583f4efe
ZigaSajovic <ziga.sajovic@gmail.com>
2019-10-25 07:01:20 +0200

    FindIf works on iterators

--- CppML/Sequence.hpp
+++ CppML/Sequence.hpp
@@ -169,32 +169,34 @@ template <typename Pipe = ml::Identity> struct Reduce {
       static_cast<bool>(sizeof...(Ts))>::template f<Init, F, Ts...>>;
 };
 
+/*
+ * Implementation of FindIf. Only ever instantiates two types.
+ */
 namespace Implementations {
-template <bool Continue> struct FindIfOr {
-  template <typename Default, typename Predicate, typename T, typename... Ts>
+template <bool Continue> struct FindIf {
+  template <int i, typename Predicate, typename T, typename... Ts>
   using f = typename ml::IfElse<Predicate::template f<T>::value>::template f<
-      T, typename FindIfOr<static_cast<bool>(
-             sizeof...(Ts) > 1)>::template f<Default, Predicate, Ts...>>;
+      ml::Int<i>, typename FindIf<static_cast<bool>(
+                      sizeof...(Ts) > 1)>::template f<i + 1, Predicate, Ts...>>;
 };
 
-template <> struct FindIfOr<false> {
-  template <typename Default, typename Predicate, typename T>
-  using f =
-      typename ml::IfElse<Predicate::template f<T>::value>::template f<T,
-                                                                       Default>;
+template <> struct FindIf<false> {
+  template <int i, typename Predicate, typename T>
+  using f = typename ml::IfElse<Predicate::template f<T>::value>::template f<
+      ml::Int<i>, ml::Int<i + 1>>;
 };
 }; // namespace Implementations
 
 /*
- * # FindIfOr:
- * Find Element or return Default
+ * # FindIf:
+ * Returns index of the element matching the predicate
  */
-template <typename Default, typename Predicate, typename Pipe = ml::Identity>
-struct FindIfOr {
+template <typename Predicate, typename Pipe = ml::Identity> struct FindIf {
   template <typename... Ts>
-  using f = ml::Invoke<
-      Pipe, typename Implementations::FindIfOr<static_cast<bool>(
-                sizeof...(Ts) > 1)>::template f<Default, Predicate, Ts...>>;
+  using f =
+      ml::Invoke<Pipe,
+                 typename Implementations::FindIf<static_cast<bool>(
+                     sizeof...(Ts) > 1)>::template f<0, Predicate, Ts...>>;
 };
 
 /*

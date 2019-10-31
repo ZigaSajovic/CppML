bd65e8ce42662a3c25569f3fc43516d55e8d6f51
ZigaSajovic <ziga.sajovic@gmail.com>
2019-10-31 14:26:13 +0100

    Insert in terms of Head and Tail

--- CppML/Sequence/Insert.hpp
+++ CppML/Sequence/Insert.hpp
@@ -7,44 +7,17 @@
 #include "../Functional/DelayedEval.hpp"
 #include "../Functional/Invoke.hpp"
 #include "../Functional/ToList.hpp"
-#include "../Vocabulary/Const.hpp"
-#include "PackExtractor.hpp"
+#include "../Sequence/Append.hpp"
+#include "../Sequence/Drop.hpp"
+#include "../Sequence/Head.hpp"
 namespace ml {
-namespace Implementations {
-/*
- * Implementation of Insert.
- */
-template <bool Continue> struct InsertImpl {
-  template <typename I, typename T, typename start, typename end, typename Pipe,
-            typename Getter, typename... Ts>
-  using f = typename ml::DelayedEval<
-      InsertImpl<(start::value + 1 < end::value)>, sizeof...(Ts),
-      ml::Int<(start::value != I::value ? I::value : -1)>, T,
-      ml::Int<start::value + (start::value != I::value)>, end, Pipe, Getter,
-      Ts...,
-      ml::Invoke<ml::IfElse<start::value == I::value>, T,
-                 ml::Invoke<Getter, start>>>;
-};
-
-template <> struct InsertImpl<false> {
-  template <typename I, typename T, typename start, typename end, typename Pipe,
-            typename Getter, typename... Ts>
-  using f = ml::DelayedEval<Pipe, sizeof...(Ts), Ts...>;
-};
-struct Insert {
-  template <int I, typename T, typename Pipe, typename... Ts>
-  using f = typename Implementations::InsertImpl<static_cast<bool>(sizeof...(
-      Ts))>::template f<ml::Int<I>, T, ml::Int<0>, ml::Int<sizeof...(Ts)>, Pipe,
-                        ml::PackExtractor<Ts...>>;
-};
-}; // namespace Implementations
-/*
- * # Insert:
- * Inserts T as the I-th element of the paramater pack.
- */
 template <int I, typename T, typename Pipe = ml::ToList> struct Insert {
   template <typename... Ts>
-  using f = ml::Implementations::Insert::template f<I, T, Pipe, Ts...>;
+  using f = ml::Invoke<
+      ml::Drop<I, ml::Append<ml::Invoke<ml::Append<ml::DelayedEval<
+                                            ml::Head<I>, sizeof...(Ts), Ts...>>,
+                                        T>>>,
+      Ts...>;
 };
 } // namespace ml
 #endif

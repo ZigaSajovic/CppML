f4ab11006ce6b79ad539bf394067df81cf743e20
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-01 12:19:04 +0100

    Pack

--- CppML/Pack.hpp
+++ CppML/Pack.hpp
@@ -2,14 +2,17 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef CPPML_SEQUENCE_HPP
-#define CPPML_SEQUENCE_HPP
+#ifndef CPPML_PACK_HPP
+#define CPPML_PACK_HPP
 #include "Pack/Append.hpp"
 #include "Pack/Drop.hpp"
 #include "Pack/Get.hpp"
+#include "Pack/Head.hpp"
+#include "Pack/Insert.hpp"
 #include "Pack/Length.hpp"
 #include "Pack/Merge.hpp"
 #include "Pack/PackExtractor.hpp"
 #include "Pack/Prepend.hpp"
 #include "Pack/Range.hpp"
+#include "Pack/Tail.hpp"
 #endif

24ec8936cf9e830ced0696b4219621cb711fcd97
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-04 23:32:12 +0100

    NoneOf

---
+++ CppML/Algorithm/NoneOf.hpp
@@ -0,0 +1,30 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CPPML_NONE_OF_HPP
+#define CPPML_NONE_OF_HPP
+#include "../Functional/Identity.hpp"
+#include "../Vocabulary/Const.hpp"
+#include "FindIf.hpp"
+namespace ml {
+/*
+ * NoneOf:
+ * Checks if none of the values satisfy the predicate
+ */
+#ifdef __cpp_fold_expressions
+template <typename Predicate, typename Pipe = Identity> struct NoneOf {
+  template <typename... Ts>
+  using f = typename Pipe::template f<
+      Bool<(!Predicate::template f<Ts>::value && ...)>>;
+};
+#else
+template <typename Predicate, typename Pipe = ml::Identity> struct NoneOf {
+  template <typename... Ts>
+  using f =
+      ml::Invoke<Pipe, ml::Bool<(ml::Invoke<ml::FindIf<Predicate>,
+                                            Ts...>::value == sizeof...(Ts))>>;
+};
+#endif
+} // namespace ml
+#endif

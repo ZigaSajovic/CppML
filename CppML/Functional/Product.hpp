f2286b955bfbe1e83f3ccbf7600a7767e696e461
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-05 23:08:58 +0100

    Product of metafunctions

---
+++ CppML/Functional/Product.hpp
@@ -0,0 +1,34 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CPPML_PRODUCT_HPP
+#define CPPML_PRODUCT_HPP
+#include "../Algorithm/Pivot.hpp"
+#include "DelayedEval.hpp"
+#include "F.hpp"
+#include "Invoke.hpp"
+
+namespace ml {
+namespace Implementations {
+template <typename Pipe, typename... Fs> struct Product {
+  template <typename... Ts>
+  using f = ml::DelayedEval<Pipe, sizeof...(Ts), ml::Invoke<Fs, Ts>...>;
+};
+} // namespace Implementations
+/*
+ * Product:
+ * Creates a product of metafunctions. Meaning:
+ * ```
+ * F_i: T -> U
+ * Product : Ts ... -> U... ; |U...| = |F_i...|
+ * ```
+ * **Note** that the last
+ * metafunction will be treated as **Pipe**.
+ */
+template <typename... Fs>
+struct Product
+    : ml::Invoke<ml::Pivot<sizeof...(Fs) - 1, ml::F<Implementations::Product>>,
+                 Fs...> {};
+} // namespace ml
+#endif

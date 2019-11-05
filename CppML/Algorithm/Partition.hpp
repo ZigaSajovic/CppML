302a6de0b356242213eb54615b753ca425022508
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-05 23:41:01 +0100

    Partition

---
+++ CppML/Algorithm/Partition.hpp
@@ -0,0 +1,19 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CPPML_PARTITION_HPP
+#define CPPML_PARTITION_HPP
+#include "../Functional/ProductMap.hpp"
+#include "Filter.hpp"
+#include "RemoveIf.hpp"
+
+namespace ml {
+/*
+ * # Partition:
+ * Paritions the parameter pack, given a predicate, into two Lists.
+ */
+template <typename Predicate, typename Pipe = ml::ToList>
+struct Partition
+    : ml::ProductMap<ml::Filter<Predicate>, ml::RemoveIf<Predicate>, Pipe> {};
+} // namespace ml

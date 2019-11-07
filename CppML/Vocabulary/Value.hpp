3e70c8a53a4c41bf3f87fa14be606623dcc54915
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-07 16:16:28 +0100

    Value (forgot the rename)

--- CppML/Vocabulary/Value.hpp
+++ CppML/Vocabulary/Value.hpp
@@ -2,24 +2,24 @@
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef CPPML_CONST_HPP
-#define CPPML_CONST_HPP
+#ifndef CPPML_VALUE_HPP
+#define CPPML_VALUE_HPP
 namespace ml {
 /*
- * Const:
+ * Value:
  * Represents a typed value
  *
  */
-template <typename T, T t> struct Const {
+template <typename T, T t> struct Value {
   using type = T;
   static constexpr T value = t;
 };
 
-template <int N> using Int = Const<int, N>;
+template <int N> using Int = Value<int, N>;
 
-template <bool N> using Bool = Const<bool, N>;
+template <bool N> using Bool = Value<bool, N>;
 
-template <char C> using Char = Const<char, C>;
+template <char C> using Char = Value<char, C>;
 
 }; // namespace ml
 #endif

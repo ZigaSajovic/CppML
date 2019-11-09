d939a153a64aed24d273a659899fdb107c1d3e78
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-09 17:17:43 +0100

    docs: Any

--- CppML/Vocabulary.hpp
+++ CppML/Vocabulary.hpp
@@ -4,8 +4,8 @@
 
 #ifndef CPPML_VOCABUARY_HPP
 #define CPPML_VOCABUARY_HPP
-#include "Vocabulary/Value.hpp"
+#include "Vocabulary/Any.hpp"
 #include "Vocabulary/List.hpp"
 #include "Vocabulary/None.hpp"
-#include "Vocabulary/Wildcard.hpp"
+#include "Vocabulary/Value.hpp"
 #endif
---
+++ docs/api/Vocabulary/Any.md
@@ -0,0 +1,10 @@
+# `<CppML/Vocabulary/Any.hpp>`
+
+## `Any`
+
+```c++
+struct Any {};
+```
+### `Any`
+
+`Any` represents *any* types (i.e. a wildcard).

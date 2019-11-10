84e1bc68bdb5fe5928728bde187c20172154aff2
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-10 15:36:06 +0100

    switched structure

---
+++ include/CppML/CppML.hpp
@@ -0,0 +1,14 @@
+//          Copyright Žiga Sajovic, XLAB 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CPPML_HPP
+#define CPPML_HPP
+#include "./Algorithm.hpp"
+#include "./Arithmetic.hpp"
+#include "./ControlFlow.hpp"
+#include "./Functional.hpp"
+#include "./Pack.hpp"
+#include "./TypeTraits.hpp"
+#include "./Vocabulary.hpp"
+#endif

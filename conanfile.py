9daa01b20021fda88ea09405fa4f764017c0e192
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-14 03:52:49 +0100

    connanfile

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake
+
+
+class CppMLConan(ConanFile):
+  name = "CppML"
+  version = "0.1"
+  license = "MIT"
+  author = "Ziga Sajovic -- ziga.sajovic+conan@xlab.si"
+  url = "https://github.com/bernedom/SI"
+  description = "A Meta Language for C++."
+  topics = ("metaprogramming", "meta-programming",
+            "template-meta-programming", "cplusplus-library")
+  exports_sources = "include/*", "CMakeLists.txt", "cmake/CppMLConfig.cmake.in", "LICENSE"
+  no_copy_source = True
+
+  def _configure_cmake(self):
+    cmake = CMake(self)
+    cmake.configure()
+    return cmake
+
+  def build(self):
+    cmake = self._configure_cmake()
+    cmake.build()
+
+  def test(self):
+    cmake = self._configure_cmake()
+    cmake.test()
+
+  def package(self):
+    cmake = self._configure_cmake()
+    cmake.install()
+
+  def package_id(self):
+    self.info.header_only()

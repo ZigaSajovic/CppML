08604b03566f8775b2bf98f90974c7f0014d0104
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-16 21:23:07 +0100

    test: Drop expanded

--- test/src/Pack/Drop.hpp
+++ test/src/Pack/Drop.hpp
@@ -4,26 +4,23 @@ namespace DropTest {
 template <typename... Ts> struct List {};
 void run() {
   {
-    using T = ml::f<ml::Drop<2, ml::F<List>>, int, int, char, bool>;
-    static_assert(std::is_same<T, List<char, bool>>::value,
-                  "Drop with a non-empty pack 1");
-  }
-  {
-    using T = ml::f<ml::Drop<3, ml::F<List>>, int, int, int, char, bool>;
-    static_assert(std::is_same<T, List<char, bool>>::value,
-                  "Drop with a non-empty pack 2");
-  }
-  {
     using T =
-        ml::f<ml::Drop<5, ml::F<List>>, int, int, int, int, int, char, bool>;
-    static_assert(std::is_same<T, List<char, bool>>::value,
-                  "Drop with a non-empty pack 3");
-  }
-  {
-    using T = ml::f<ml::Drop<7, ml::F<List>>, int, int, int, int, int, int, int,
-                    char, bool>;
-    static_assert(std::is_same<T, List<char, bool>>::value,
-                  "Drop with a non-empty pack 3");
+        ml::f<ml::Drop<63, ml::F<List>>, ml::Int<0>, ml::Int<1>, ml::Int<2>,
+              ml::Int<3>, ml::Int<4>, ml::Int<5>, ml::Int<6>, ml::Int<7>,
+              ml::Int<8>, ml::Int<9>, ml::Int<10>, ml::Int<11>, ml::Int<12>,
+              ml::Int<13>, ml::Int<14>, ml::Int<5>, ml::Int<16>, ml::Int<17>,
+              ml::Int<18>, ml::Int<19>, ml::Int<20>, ml::Int<21>, ml::Int<22>,
+              ml::Int<23>, ml::Int<24>, ml::Int<25>, ml::Int<26>, ml::Int<27>,
+              ml::Int<28>, ml::Int<29>, ml::Int<30>, ml::Int<31>, ml::Int<32>,
+              ml::Int<33>, ml::Int<34>, ml::Int<35>, ml::Int<36>, ml::Int<37>,
+              ml::Int<38>, ml::Int<39>, ml::Int<40>, ml::Int<41>, ml::Int<42>,
+              ml::Int<43>, ml::Int<44>, ml::Int<45>, ml::Int<46>, ml::Int<47>,
+              ml::Int<48>, ml::Int<49>, ml::Int<50>, ml::Int<51>, ml::Int<52>,
+              ml::Int<53>, ml::Int<54>, ml::Int<55>, ml::Int<56>, ml::Int<57>,
+              ml::Int<58>, ml::Int<59>, ml::Int<60>, ml::Int<61>, ml::Int<62>,
+              ml::Int<63>, ml::Int<64>>;
+    static_assert(std::is_same<T, List<ml::Int<63>, ml::Int<64>>>::value,
+                  "Drop with 63");
   }
   {
     using T = ml::f<ml::Drop<0, ml::F<List>>>;

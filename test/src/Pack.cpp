deb58bd2d11a77ded0d4873848117e290c241407
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-16 20:44:56 +0100

    test: front and get

--- test/src/Pack.cpp
+++ test/src/Pack.cpp
@@ -1,11 +1,15 @@
 #include "./Pack/Append.hpp"
 #include "./Pack/Concat.hpp"
 #include "./Pack/Drop.hpp"
+#include "./Pack/Front.hpp"
+#include "./Pack/Get.hpp"
 #include "./Pack/Prepend.hpp"
 
 int main() {
   AppendTest::run();
   ConcatTest::run();
   DropTest::run();
+  FrontTest::run();
+  GetTest::run();
   PrependTest::run();
 }

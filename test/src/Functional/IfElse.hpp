5ffe8562ce595f6a4c9e4de92a058d95f9337542
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-17 01:41:59 +0100

    test: IfElse

--- test/src/Functional/IfElse.hpp
+++ test/src/Functional/IfElse.hpp
@@ -9,7 +9,7 @@ void run() {
   }
   {
     using T = ml::f<ml::IfElse<ml::Bool<false>, ml::F<R>>, int, char>;
-    static_assert(std::is_same<T, R<char>>::value, "IfElse true");
+    static_assert(std::is_same<T, R<char>>::value, "IfElse false");
   }
 }
 } // namespace IfElseTest

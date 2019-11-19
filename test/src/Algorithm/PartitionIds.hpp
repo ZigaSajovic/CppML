a88dba8b652f33c9b1c7bded90471dce70ea4cd5
ZigaSajovic <ziga.sajovic@gmail.com>
2019-11-19 17:29:13 +0100

    tests: Partition tests are now architecture independent

--- test/src/Algorithm/PartitionIds.hpp
+++ test/src/Algorithm/PartitionIds.hpp
@@ -1,19 +1,31 @@
 #include "CppML/CppML.hpp"
 
 namespace PartitionIdsTest {
-template <typename T> using AlignPart = ml::Bool<(alignof(T) > 4)>;
+template <typename T> using Size = ml::Bool<(sizeof(T) > 4)>;
 template <typename... Ts> struct List {};
+struct Type0 {
+  char _[4];
+};
+struct Type1 {
+  char _[6];
+};
+struct Type2 {
+  char _[4];
+};
+struct Type3 {
+  char _[6];
+};
 void run() {
   {
-    using T = ml::f<ml::PartitionIds<ml::F<AlignPart>, ml::F<List>>, int, long,
-                    char, double>;
+    using T = ml::f<ml::PartitionIds<ml::F<Size>, ml::F<List>>, Type0, Type1,
+                    Type2, Type3>;
     static_assert(
         std::is_same<T, List<ml::ListT<ml::Int<1>, ml::Int<3>>,
                              ml::ListT<ml::Int<0>, ml::Int<2>>>>::value,
         "PartitionIds with a non-empty pack");
   }
   {
-    using T = ml::f<ml::PartitionIds<ml::F<AlignPart>, ml::F<List>>>;
+    using T = ml::f<ml::PartitionIds<ml::F<Size>, ml::F<List>>>;
     static_assert(std::is_same<T, List<ml::ListT<>, ml::ListT<>>>::value,
                   "PartitionIds with an empty pack");
   }
